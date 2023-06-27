af21eb049908372474cf19e0eb7f472d6c3538ee
Krishna Kumar <93042057+krishnakumar1204@users.noreply.github.com>
2023-06-28 03:00:08 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,12 +22,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    Discover a world of cinematic wonders with our movie catalogue app. From timeless classics to the latest blockbusters, we've got your film cravings covered. Browse, search, and curate your own personal watchlist effortlessly. Lights, camera, action! Start your movie adventure today.
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
